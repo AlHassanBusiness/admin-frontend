deb8561aec2912da012d0986b5c48109e0f6a2be
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-06-27 14:06:45 +0500

    changing api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://amazoon-backend.amazoonaustralia.com',
+    baseURL: 'https://backend.amazoonaustralia.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
